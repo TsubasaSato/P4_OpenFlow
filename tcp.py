012e679571cf17b04333cb652edf589bb4dcc6ea
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-11-30 20:53:26 +0900

    Create tcp.py

---
+++ tcp.py
@@ -0,0 +1,43 @@
+from ryu.utils import binary_str
+from ryu.lib.packet import packet
+from ryu.lib.packet import in_proto
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import tcp
+
+pkt_in = packet.Packet()
+pkt_in.add_protocol(
+    ethernet.ethernet(
+        dst='11:22:33:44:55:66',
+        src='aa:bb:cc:dd:ee:ff',
+    ),
+)
+pkt_in.add_protocol(
+    ipv4.ipv4(
+        dst='10.0.0.1',
+        src='10.0.0.2',
+        proto=in_proto.IPPROTO_TCP,
+    ),
+)
+pkt_in.add_protocol(
+    tcp.tcp(
+        src_port=50080,
+        dst_port=80,
+    ),
+)
+payload_data = b'arbitrary'  # as a raw binary
+pkt_in.add_protocol(payload_data)
+pkt_in.serialize()
+
+print('*** constructed packet')
+print(pkt_in)
+
+print('*** binary of constructed packet')
+print(binary_str(pkt_in.data))
+
+print('*** parsed packet')
+pkt_out = packet.Packet(pkt_in.data)
+print(pkt_out)
+
+print('*** get payload of TCP')
+print(pkt_out.protocols[-1])
