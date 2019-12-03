045e2e9a00e4537dea3e6867e7824f804839d44f
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-12-03 13:22:59 +0900

    Update TcpSyn.py

--- TcpSyn.py
+++ TcpSyn.py
@@ -23,7 +23,7 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-from ryu.lib.packet import ip
+from ryu.lib.packet import ipv4
 from ryu.lib.packet import tcp
 
 class TCPSYN13(app_manager.RyuApp):
