1c2869e1ffa432ebe71acaa23ab7a9bfe4ac4b13
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-12-07 12:24:38 +0900

    Update TcpSyn.py

--- TcpSyn.py
+++ TcpSyn.py
@@ -20,7 +20,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.ofproto import inet
+from ryu.ofproto import inet
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
