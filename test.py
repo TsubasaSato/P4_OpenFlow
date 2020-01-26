a4a32117ff5943ba53146ae2ea44f11900d689f0
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-26 16:02:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,18 +9,19 @@ def run_and_capture(cmd):
     # ここでプロセスが (非同期に) 開始する.
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     buf = []
+    a=[b'loading app TcpSyn_comment.py\n', b'loading app ryu.controller.ofp_handler\n', b'instantiating app TcpSyn_comment.py of TCPSYN13\n', b'instantiating app ryu.controller.ofp_handler of OFPHandler\n', b'BRICK TCPSYN13\n', b'  CONSUMES EventOFPPacketIn\n', b'  CONSUMES EventOFPSwitchFeatures\n', b'BRICK ofp_event\n', b"  PROVIDES EventOFPPacketIn TO {'TCPSYN13': {'main'}}\n", b"  PROVIDES EventOFPSwitchFeatures TO {'TCPSYN13': {'config'}}\n", b'  CONSUMES EventOFPEchoReply\n', b'  CONSUMES EventOFPEchoRequest\n', b'  CONSUMES EventOFPErrorMsg\n', b'  CONSUMES EventOFPHello\n', b'  CONSUMES EventOFPPortDescStatsReply\n', b'  CONSUMES EventOFPPortStatus\n', b'  CONSUMES EventOFPSwitchFeatures\n']
+
 
     while True:
         # バッファから1行読み込む.
         line = proc.stdout.readline()
         buf.append(line)
-        print(buf)
 
         # バッファが空 + プロセス終了.
-        if not line and proc.poll() is not None:
+        if buf==a:
             break
-
-    return ''.join(buf)
+    proc.terminate()
+    print("terminate()")
 
 data=[]
 for _ in range(100):
