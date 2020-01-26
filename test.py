2897a052c6628b89fdf17c036e6569710fd070be
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2020-01-26 15:58:04 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
 import subprocess,time
 
+def run_and_capture(cmd):
+    '''
+    :param cmd: str 実行するコマンド.
+    :rtype: str
+    :return: 標準出力.
+    '''
+    # ここでプロセスが (非同期に) 開始する.
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    buf = []
+
+    while True:
+        # バッファから1行読み込む.
+        line = proc.stdout.readline()
+        buf.append(line)
+        print(buf)
+
+        # バッファが空 + プロセス終了.
+        if not line and proc.poll() is not None:
+            break
+
+    return ''.join(buf)
+
 data=[]
 for _ in range(100):
     t1=time.time()
-    subprocess.call(["./scripts/script_ryu-manager.sh"])
+    run_and_capture(["./scripts/script_ryu-manager.sh"])
     t2=time.time()
     
     print(t2-t1)
